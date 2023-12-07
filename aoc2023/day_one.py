42dae8a61dee628645a2793fc99f53cf6d0f18f4
Roy Straub <roy@codecraftr.nl>
2023-12-07 22:04:46 +0100

    Solved day one

--- aoc2023/day_one.py
+++ aoc2023/day_one.py
@@ -1,2 +1,34 @@
+#!/usr/bin/env python3
+
+
 def day_one(coordinates: str) -> int:
-    return 1
+    lines = parse_coordinates(coordinates)
+
+    numbers_per_lines = list(map(first_last_number_in_line, lines))
+
+    return sum(numbers_per_lines)
+
+
+def parse_coordinates(text: str) -> list[str]:
+    lines = text.split("\n")
+
+    stripped = list(map(lambda line: line.replace(" ", ""), lines))
+    non_blank = list(filter(lambda line: line != "", stripped))
+    return non_blank
+
+
+def first_last_number_in_line(line: str) -> int:
+    numbers = [char for char in line if char.isdigit()]
+    first_number = numbers[0]
+    last_number = numbers[-1]
+    return int(first_number + last_number)
+
+
+def run():
+    with open('input.txt', 'r') as file:
+        content = file.read()
+        print(day_one(content))
+
+
+if __name__ == "__main__":
+    run()
